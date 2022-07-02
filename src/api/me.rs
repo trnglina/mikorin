60a77de84a719f47b6c1634a314351e98c5bcd1c
Teng Liu <teng@trnglina.org>
2022-07-01 20:25:55 -0600

    Simplify /me implementation

--- src/api/me.rs
+++ src/api/me.rs
@@ -1,43 +1,11 @@
-use axum::{http::StatusCode, routing::get, Extension, Router};
-use axum_extra::extract::CookieJar;
-use sqlx::{Pool, Postgres};
+use axum::{response::Redirect, routing::get, Router};
 
-use crate::{
-    config,
-    model::User,
-    utility::{action, ApiResult},
-};
+use crate::{auth::Authenticated, config};
 
 pub fn controllers() -> Router {
     Router::new().route("/", get(show_me))
 }
 
-async fn show_me(
-    Extension(pool): Extension<Pool<Postgres>>,
-    jar: CookieJar,
-) -> ApiResult<action::Show<User>> {
-    if let Some(session_id) = jar
-        .get(config::COOKIE_SESSION_KEY)
-        .map(|x| x.value().to_string())
-    {
-        Ok(action::Show(
-            sqlx::query_as!(
-                User,
-                r#"
-                SELECT Users.id, Users.name
-                FROM Sessions JOIN Users ON Sessions.user_id = Users.id
-                WHERE Sessions.id = $1
-                "#,
-                session_id,
-            )
-            .fetch_one(&pool)
-            .await
-            .map_err(|err| match err {
-                sqlx::Error::RowNotFound => StatusCode::FORBIDDEN,
-                _ => internal_error!(err),
-            })?,
-        ))
-    } else {
-        Err(StatusCode::FORBIDDEN)
-    }
+async fn show_me(Authenticated(user_id): Authenticated) -> Redirect {
+    Redirect::to(&format!("{}/users/{}", config::API_ROUTE, user_id))
 }
